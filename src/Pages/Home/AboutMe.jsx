a362b7740475104af4fef29bea4b76f9350be9ce
Ricardo Philippe <philippericardo26@gmail.com>
2024-10-08 17:01:51 -0700

    updating about me for second time

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -9,14 +9,9 @@ export default function AboutMe() {
           <p className="section--title">About</p>
           <h1 className="skills-section--heading">About Me</h1>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae? Voluptatem, reprehenderit alias? Eligendi aperiam
-            tempora numquam sint odit optio.
+          Hi, I am Ricardo Philippe, a 37-year-old passionate web developer with a love for crafting digital experiences that are both functional and visually compelling. I am a husband and father of two, which means my life is as full of adventure at home as it is in my coding projects!
           </p>
           <p className="hero--section-description">
-          Hi, I am Ricardo Philippe, a 37-year-old passionate web developer with a love for crafting digital experiences that are both functional and visually compelling. I am a husband and father of two, which means my life is as full of adventure at home as it is in my coding projects!
-
           In November 2024, I earned my Full Stack Web Development Certificate from the University of Central Florida, where I honed my skills in both front-end and back-end technologies. Whether I am building responsive websites, developing dynamic web applications, or integrating complex databases, I am always focused on creating solutions that meet both client and user needs.
 
           With every project, I bring a blend of creativity, technical expertise, and problem-solving to the table. When I am not coding, you will find me spending time with my family or exploring the latest tech trends.
